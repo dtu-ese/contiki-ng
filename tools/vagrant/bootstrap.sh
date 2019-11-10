2ceba8e5424e30f8258b1849fae61d9220b0fac6
George Oikonomou <g.oikonomou@bristol.ac.uk>
2019-11-10 14:24:13 +0000

    Vagrant bootstrap: Reduce wget verbosity for arm-gcc
    Speeds up provisioning and makes the log cleaner

--- tools/vagrant/bootstrap.sh
+++ tools/vagrant/bootstrap.sh
@@ -13,7 +13,7 @@ sudo apt-get clean
 sudo python2 -m pip install intelhex
 
 # Install ARM toolchain
-wget https://launchpad.net/gcc-arm-embedded/5.0/5-2015-q4-major/+download/gcc-arm-none-eabi-5_2-2015q4-20151219-linux.tar.bz2
+wget -nv https://launchpad.net/gcc-arm-embedded/5.0/5-2015-q4-major/+download/gcc-arm-none-eabi-5_2-2015q4-20151219-linux.tar.bz2
 tar xjf gcc-arm-none-eabi-5_2-2015q4-20151219-linux.tar.bz2 -C /tmp/
 sudo cp -f -r /tmp/gcc-arm-none-eabi-5_2-2015q4/* /usr/local/
 rm -rf /tmp/gcc-arm-none-eabi-* gcc-arm-none-eabi-*-linux.tar.bz2
